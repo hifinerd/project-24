8c1ac4ead38d457a797431ab822f1cf3d6223efc
hifinerd <69434858+hifinerd@users.noreply.github.com>
2020-12-02 12:26:03 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var groundSprite;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	//Create the Bodies Here.
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+//	World.add(world, ground);
+	//paper = new Paper(20,50,25)
+	groundSprite=createSprite(width/2, height-300, width,10);
+
+
+   Engine.run(engine);
+  
+}
+
+
+function draw() {
+//  rectMode(CENTER);
+  background(0);
+  //paper.display();
+drawSprites();
+}
+
+
+
