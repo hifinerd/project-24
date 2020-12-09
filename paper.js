f240264eefd7fa0f24f8110d9902c8a3a799398f
hifinerd <69434858+hifinerd@users.noreply.github.com>
2020-12-09 13:03:05 -0500

    Add files via upload

--- paper.js
+++ paper.js
@@ -3,24 +3,26 @@ constructor(x,y,r)
 {
     var options={
         isStatic:false,
-        restitution:0.3,
+        restitution:0.8,
         friction:0.5,
-        density:1.2
+	    density:1.2
     }
 
     this.x=x;
     this.y=y;
     this.r=r
-    this.body=Bodies.circle(x,y,r)
+    this.body=Bodies.circle(x,y,r,options)
     World.add(world,this.body)
 }
 display()
 {
     var paperpos=this.body.position;
+    console.log(paperpos)
     push()
     translate(paperpos.x,paperpos.y);
-    rectMode(CENTER)
+   
     strokeWeight(3);
+    ellipseMode(RADIUS);
     ellipse(0,0,this.r, this.r)
     pop()
 }
--- sketch.js
+++ sketch.js
@@ -18,10 +18,13 @@ function setup() {
 	world = engine.world;
 	
 	//Create the Bodies Here.
-	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-//	World.add(world, ground);
-	//paper = new Paper(20,50,25)
-	groundSprite=createSprite(width/2, height-300, width,10);
+	ground = Bodies.rectangle(width/2, height-20, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	paper = new Paper(100,50,25)
+	bottombin = new Bin(650,665,200,20)
+	leftbin = new Bin(550,600,20,150)
+	rightbin = new Bin(750,600,20,150)
+	//groundSprite=createSprite(width/2, height-20, width,10);
 
 
    Engine.run(engine);
@@ -32,9 +35,22 @@ function setup() {
 function draw() {
 //  rectMode(CENTER);
   background(0);
-  //paper.display();
-drawSprites();
+  
+  rectMode(CENTER);
+ rect(ground.position.x,ground.position.y,800,10);
+ paper.display();
+ bottombin.display();
+ leftbin.display();
+ rightbin.display();
+//drawSprites();
 }
 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		//console.log(paper.body);
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:55,y:-55})
+		//console.log(paper.body);
+	}
+}
 
 
